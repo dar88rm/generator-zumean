015e71046a678826c762c397b5aad1d5b74c6e38
dar88rm <dario04051988@gmail.com>
2016-09-11 17:08:06 +0200

    Add files via upload

--- generators/app/templates/FE/app/scripts/controllers.js
+++ generators/app/templates/FE/app/scripts/controllers.js
@@ -2,13 +2,25 @@
 angular.module('thisApp')
 		
         .controller('IndexController', ['$scope','thisService', function($scope,thisService) {
-           
-           $scope.bla =  {
-                                    _id:0,
-                                    name:'Bla',
-                                    description:'Bla bla bla'
-                                };
-                                 
+            
+            $scope.pois= {};
+            
+            thisService.getPois()
+            .then(
+                function(response) {
+                    $scope.pois = response.data;
+                },
+                function(response) {
+                    
+                }
+            );
+            
+            $scope.update = function(poi) {
+                thisService.savePoi(poi);
+                $scope.pois.push(poi);
+                $scope.poi = {};
+                $scope.user_form.$setPristine();
+            };
         }])
  
         ;
--- generators/app/templates/FE/app/scripts/services.js
+++ generators/app/templates/FE/app/scripts/services.js
@@ -4,9 +4,14 @@ angular.module('thisApp')
 .constant("baseURL","http://localhost:3000/")
        
        .service('thisService', ['$http', 'baseURL', function($http,baseURL) {
-          
+            
+           this.getPois = function(){
+                    return $http.get(baseURL+"pois");
+            };
 
-    
+            this.savePoi = function(poiname){
+                $http.post(baseURL+"addpoi", poiname);
+            };
         }])
 
 		;
